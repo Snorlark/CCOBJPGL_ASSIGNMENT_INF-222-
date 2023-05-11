b51af6ce4d6a9d8ae028587cd47cc6f087878239
Lark Sigmuond A. Babao <129244163+Snorlark@users.noreply.github.com>
2023-05-11 07:28:08 +0100

    Add files via upload

---
+++ exercise3/Dog/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pokemon class
+        Lillidough myPokemonDog = new Lillidough();
+
+        myPokemonDog.bark();
+        System.out.println(myPokemonDog.ears);
+        System.out.println(myPokemonDog.color);
+    }
+}
---
+++ exercise3/Dog/Lillidough.java
@@ -0,0 +1,7 @@
+public class Lillidough extends Fidough implements Lillipup{
+    
+    public void bark() {
+        
+        System.out.println("Lillidough used thunder bark! It is not effective.");
+    }
+}
